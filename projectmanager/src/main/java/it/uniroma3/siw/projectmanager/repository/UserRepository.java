f57f859d8a3e180d8d566bf7279c0e9c5fcdf63e
andrew98_ <andreamoscato981@gmail.com>
2020-06-01 16:43:48 +0200

    prima implementazione repository e service

--- projectmanager/src/main/java/it/uniroma3/siw/projectmanager/repository/UserRepository.java
+++ projectmanager/src/main/java/it/uniroma3/siw/projectmanager/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import it.uniroma3.siw.projectmanager.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
-
+	
 }
--- projectmanager/src/main/java/it/uniroma3/siw/projectmanager/service/UserService.java
+++ projectmanager/src/main/java/it/uniroma3/siw/projectmanager/service/UserService.java
@@ -1,5 +1,32 @@
 package it.uniroma3.siw.projectmanager.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.projectmanager.model.User;
+import it.uniroma3.siw.projectmanager.repository.UserRepository;
+
+
+@Service
 public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+
+	
+		@Transactional
+		public User signUp(User user) {
+			User u = new User();
+			userRepository.save(u);
+			
+
+			
+			
+		}
+	
+
 
 }
