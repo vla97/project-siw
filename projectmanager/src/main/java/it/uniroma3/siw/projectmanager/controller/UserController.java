ff8c138a2d9948bbf6764ad7a9efa73053b562ea
andrew98_ <andreamoscato981@gmail.com>
2020-06-02 17:08:35 +0200

    prova

--- projectmanager/src/main/java/it/uniroma3/siw/projectmanager/controller/UserController.java
+++ projectmanager/src/main/java/it/uniroma3/siw/projectmanager/controller/UserController.java
@@ -1,7 +1,11 @@
 package it.uniroma3.siw.projectmanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.uniroma3.siw.projectmanager.model.User;
 import it.uniroma3.siw.projectmanager.service.UserService;
 
 public class UserController {
@@ -9,6 +13,10 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	//@RequestMapping(value
+	@RequestMapping(value = "/Inseriscidati", method = RequestMethod.GET)
+	public String inserisciDati(Model model) {
+		model.addAttribute("user", new User());	
+		return "userForm.html";
+	}
 
 }
