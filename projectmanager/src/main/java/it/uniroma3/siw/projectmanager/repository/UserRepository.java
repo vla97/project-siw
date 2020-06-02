f72739c9f3fae22e111f07fb3429c689eb769bd7
vla97 <vla.petriv@stud.uniroma3.it>
2020-06-02 10:12:40 +0200

    add UserService

--- projectmanager/src/main/java/it/uniroma3/siw/projectmanager/repository/UserRepository.java
+++ projectmanager/src/main/java/it/uniroma3/siw/projectmanager/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package it.uniroma3.siw.projectmanager.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.projectmanager.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
+	
+	public Optional<User> findByUserName(String username);
 	
 }
--- projectmanager/src/main/java/it/uniroma3/siw/projectmanager/service/UserService.java
+++ projectmanager/src/main/java/it/uniroma3/siw/projectmanager/service/UserService.java
@@ -1,5 +1,9 @@
 package it.uniroma3.siw.projectmanager.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +18,50 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+		
+		//trova utente per id
+		@Transactional
+		public User getUser(long id) {
+			Optional<User> r = this.userRepository.findById(id);
+			return r.orElse(null);
+		}
+		
+		//Trova utente per username
+		
+		
+		@Transactional
+		public User getUser(String username) {
+			
+			Optional<User> r = this.userRepository.findByUserName(username);
+			return r.orElse(null);
+		}
+			
+		// Salva utente nel DB
+		
+		@Transactional
+		public User saveUser(User user) {
+			return this.userRepository.save(user);
+		}
+		
+		//visualizza tutti i utenti
+		
+		@Transactional
+		public List<User> getAllUsers(){
+			List<User> r = new ArrayList<>();
+			Iterable<User>  i = this.userRepository.findAll();
+			for(User user : i)
+				r.add(user);
+			return r;
+		}
+		
 
 
 	
-		@Transactional
-		public User signUp(User user) {
-			User u = new User();
-			userRepository.save(u);
-			
-
-			
-			
-		}
+		//@Transactional
+		//public User signUp(User user) {
+		//	User u = new User();
+		//	userRepository.save(u);
+		//}
 	
 
 
