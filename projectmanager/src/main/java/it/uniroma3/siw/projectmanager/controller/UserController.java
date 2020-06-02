8c8d0dbf00bb504a35c29e99e350c7c829fc9831
andrew98_ <andreamoscato981@gmail.com>
2020-06-02 19:58:57 +0200

    prova

--- projectmanager/src/main/java/it/uniroma3/siw/projectmanager/controller/UserController.java
+++ projectmanager/src/main/java/it/uniroma3/siw/projectmanager/controller/UserController.java
@@ -1,25 +1,36 @@
 package it.uniroma3.siw.projectmanager.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import it.uniroma3.siw.projectmanager.model.User;
-import it.uniroma3.siw.projectmanager.service.UserService;
+import it.uniroma3.siw.projectmanager.model.Project;
+import it.uniroma3.siw.projectmanager.service.ProjectService;
 
 
 @Controller
 public class UserController {
-	//private UserValidator userValidator;
+	// private UserValidator userValidator;
 	@Autowired
-	private UserService userService;
-	
-	@RequestMapping(value = "/inserisci", method = RequestMethod.GET)
+	private ProjectService projectService;
+
+	@RequestMapping(value = "/creaProgetto", method = RequestMethod.GET)
 	public String inserisciDati(Model model) {
-		model.addAttribute("user", new User());	
-		return "formUser.html";
+		model.addAttribute("project", new Project());	
+		return "formProgetto.html";
 	}
+	
+	@RequestMapping(value = "/salva", method = RequestMethod.GET)
+	public String salvaProgetto(Model model) {
+		Project project = (Project) model.getAttribute("project");
+		projectService.salvaProgetto(project);
+		return "index.html";
+	}
+
 
 }
