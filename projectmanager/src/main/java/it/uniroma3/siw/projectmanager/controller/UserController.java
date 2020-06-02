c2e33e81d9e318a85e9dfd0dc24d9d697648f6ac
vla97 <vla.petriv@stud.uniroma3.it>
2020-06-02 20:12:46 +0200

    .

--- projectmanager/src/main/java/it/uniroma3/siw/projectmanager/controller/UserController.java
+++ projectmanager/src/main/java/it/uniroma3/siw/projectmanager/controller/UserController.java
@@ -4,8 +4,7 @@ package it.uniroma3.siw.projectmanager.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
