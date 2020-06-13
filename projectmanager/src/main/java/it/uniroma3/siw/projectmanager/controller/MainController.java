0be59bf8e746e49e8abe931476dac09b4b641f85
andrew98_ <andreamoscato981@gmail.com>
2020-06-13 13:12:49 +0200

    prova

--- projectmanager/src/main/java/it/uniroma3/siw/projectmanager/controller/MainController.java
+++ projectmanager/src/main/java/it/uniroma3/siw/projectmanager/controller/MainController.java
@@ -1,6 +1,7 @@
 package it.uniroma3.siw.projectmanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,6 +12,15 @@ import it.uniroma3.siw.projectmanager.controller.session.SessionData;
 import it.uniroma3.siw.projectmanager.model.User;
 import it.uniroma3.siw.projectmanager.service.ProjectService;
 import it.uniroma3.siw.projectmanager.service.UserService;
+import static it.uniroma3.siw.projectmanager.model.Credenziali.ADMIN_ROLE;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
 
 @Controller
 public class MainController {
@@ -32,6 +42,27 @@ public class MainController {
 		model.addAttribute("user", user);
 		return "home";
 	}
+	
+	@RequestMapping(value = { "/tornaIndietro" }, method = RequestMethod.GET)
+	public void tornaIndietro(Model model, HttpServletRequest request,
+		      HttpServletResponse response, Authentication authentication) throws IOException, ServletException{
+		 
+		
+			boolean admin = false;
+	        
+	        
+	        for (GrantedAuthority auth : authentication.getAuthorities()) {
+	            if (ADMIN_ROLE.equals(auth.getAuthority())){
+	              admin = true;
+	            }
+	        }
+	        
+	        if(admin){
+	          response.sendRedirect("/admin");
+	        }else{
+	          response.sendRedirect("/home");
+	        }
+	  }
 
 	
 }
